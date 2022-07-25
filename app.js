48b7ff459e28c4ed505c875260b5e5538e51be87
Muhammad Ismail <connect.ismail@protonmail.com>
2022-07-25 20:07:53 +0530

    CRUD done- v1

--- app.js
+++ app.js
@@ -14,13 +14,17 @@ app.use(express.static('./dist/voice-of-movies-frontend'));
 
 const validateObjectId = (id) => ObjectId.isValid(id) && (new ObjectId(id)).toString() === id; //true or false
 
+
+// -----------------------ALL ROUTES BEGIN--------------------------
+
+
 app.delete('/api/movies/delete/:id', (req,res)=> {
-  
-  id = req.params.id;
-  // console.log(id);
-  MovieData.findByIdAndDelete({"_id":id})
+  movieId = req.params.id;
+  console.log('DELETE: ',movieId);
+
+  MovieData.findByIdAndDelete({"_id":movieId})
   .then(()=>{
-      console.log('deleted',id)
+      console.log('deleted',movieId)
       res.json({
         'status': true,
         'response': 'Successfully Deleted'
@@ -28,13 +32,45 @@ app.delete('/api/movies/delete/:id', (req,res)=> {
     });
 });
 
+app.put('/api/movies/update', (req, res)=> {
+  console.log('PUT: Edit Movie -', req.body);
+  var id = req.body.movie._id;
+
+  var movie = {       
+    movieId : req.body.movie.movieId,
+    movieName : req.body.movie.movieName,
+    director: req.body.movie.director,
+    articleAuthor : req.body.movie.articleAuthor,
+    language : req.body.movie.language,
+    year : req.body.movie.year,
+    genre : req.body.movie.genre,
+    description : req.body.movie.description,
+    starRating : req.body.movie.starRating,
+    likes: req.body.movie.likes,
+    imageUrl : req.body.movie.imageUrl
+  }
+
+  MovieData.findByIdAndUpdate({"_id":id}, {$set: movie}, {useFindAndModify: false})
+    .then((data)=> {
+      // console.log('returned',d);
+      res.json({
+        'endpoint' : '/api/movies/update',
+        'status' : true,
+        'response' : 'Edit Success'
+      });
+    });
+
+});
+
 app.post('/api/movies/insert', (req, res)=> {
+  console.log('POST: Add Movie -',req.body)
  
   var movie = {       
     movieId : req.body.movie.movieId,
     movieName : req.body.movie.movieName,
     director: req.body.movie.director,
     articleAuthor : req.body.movie.articleAuthor,
+    language : req.body.movie.language,
     year : req.body.movie.year,
     genre : req.body.movie.genre,
     description : req.body.movie.description,
@@ -57,7 +93,7 @@ app.get('/api/movies/:id', (req, res)=> {
     MovieData.findOne({"_id":id})
     .then((movie)=>{
       if(!!movie) {
-        console.log(`GET: View movie: ${movie.movieName}`);
+        console.log(`GET: Get a movie: ${movie.movieName}`);
         res.json({
           'endpoint' : '/api/movies/:id',
           'status' : true,
@@ -72,24 +108,21 @@ app.get('/api/movies/:id', (req, res)=> {
       }
     });
   } else {
-  console.log('Invalid Object Id');
-  res.json({
-    'endpoint' : '/api/movies/:id',
-    'status' : false,
-    'response' : 'Invalid Object id'
-  });
-
+    console.log('Invalid Object Id');
+    res.json({
+      'endpoint' : '/api/movies/:id',
+      'status' : false,
+      'response' : 'Invalid Object id'
+    });
   }
 
 });
-
-
 
 app.get('/api/movies', (req,res)=> {
     
   MovieData.find()
     .then((movies)=> {
-      console.log('Get: All movies')
+      console.log('GET: All movies')
       if(movies.length != 0) {
         res.json({
           'endpoint' : '/api/movies',
@@ -115,6 +148,9 @@ app.get("/", (req, res)=> {
 // app.get('/*', (req, res)=> {
 //   res.sendFile(path.join(__dirname + './dist/voice-of-movies-frontend/index.html'));
 // });
+
+// -----------------------ALL ROUTES END--------------------------
+
 
 const PORT = process.env.PORT || 3333;
 
